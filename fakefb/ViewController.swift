2aeb991740ca48a347abb66b89485d98795d07cf
Ondrej Rohon <ondrej.rohon@gmail.com>
2025-09-19 16:28:07 +0200

    Initial Commit

---
+++ fakefb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fakefb
+//
+//  Created by Ondrej Rohon on 19/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
